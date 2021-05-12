f9b100a24202fba9ea0ae6825b0a1f1acbd356d0
Priya Khambhayta <priyagajjar@hotmail.com>
2021-05-12 16:55:34 +1000

    Initial Commit

---
+++ PropertyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PropertyApp
+//
+//  Created by Priya Gajjar on 12/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
